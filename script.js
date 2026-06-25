637cb51f3839fb1ff617a78899267f2a214e8ef3
Superex123 <shaninviktor0905@gmail.com>
2026-06-25 14:24:59 +0200

    Added Participants List

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let participants = [];
+
 registrationForm.addEventListener("submit", function(event) {
     event.preventDefault();
     const user = document.getElementById("Fullname");
@@ -5,7 +7,9 @@ registrationForm.addEventListener("submit", function(event) {
     const age = document.getElementById("Age");
     const fieldOfStudy = document.getElementById("field_of_study").value;
     const terms = document.getElementById("terms");
+    const list = document.getElementById("userList");
     let message = document.getElementById("message");
+    let userCount = document.getElementById("userCount");
     message.textContent = "";
     
     if (user.value.trim() === "" || email.value.trim() === "" || email.value.includes("@") === false || age.value.trim() < 18 || !terms.checked) {
@@ -36,16 +40,28 @@ registrationForm.addEventListener("submit", function(event) {
             // alert("Please accept the terms and conditions.");
             message.textContent += "Please accept the terms and conditions.\n";
         }
-        else {
-        message.textContent = "Registration successful!\n" + user.value + "\n"+ "Age:" + age.value + "\n" + "Field of Study: " + fieldOfStudy + "\n";
-        message.style.color = "green";
     }
-    }
+    else {
+            message.textContent = "Registration successful!\n" + user.value + "\n"+ "Age:" + age.value + "\n" + "Field of Study: " + fieldOfStudy + "\n";
+            message.style.color = "green";
+            participant = {
+                name: user.value,
+                email: email.value,
+                age: age.value,
+                fieldOfStudy: fieldOfStudy
+            };
+            participants.push(participant);
+            userCount.textContent = participants.length;
+            addParticipant(participant);
+            registrationForm.reset();
 
-    
-    
-    
-    
 
-
+    }   
 });
+
+function addParticipant(participant) {
+    const list = document.getElementById("userList");
+    const listItem = document.createElement("li");
+    listItem.textContent = `Name: ${participant.name}\n Age: ${participant.age} \n Field of Study: ${participant.fieldOfStudy}`;
+    list.appendChild(listItem);
+}
